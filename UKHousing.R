ffbca0f7c348a1c8bde43bf4c3123f494592c345
Vittorio Romaniello <vittorio.romaniello@yahoo.it>
2020-02-09 08:20:57 -0800

    Add files via upload

--- UKHousing.R
+++ UKHousing.R
@@ -16,13 +16,14 @@ for (i in 1:ncol(X)){
 
 n_train = 20000
 
-accuracy_rglm = rep(0, 100)
-accuracy_rf_bigmtry = rep(0, 100)
-accuracy_rf = rep(0, 100)
-accuracy_xtree = rep(0, 100)
-accuracy_svm = rep(0, 100)
+reps = 10
+accuracy_rglm = rep(0, reps)
+accuracy_rf_bigmtry = rep(0, reps)
+accuracy_rf = rep(0, reps)
+accuracy_xtree = rep(0, reps)
+accuracy_svm = rep(0, reps)
 
-for (i in 1:100){
+for (i in 1:reps){
   idx = sample(length(y), size = n_train, replace = FALSE)
   
   X_train = X[idx, ]
@@ -32,17 +33,24 @@ for (i in 1:100){
   
   bags = 100
   model_rglm = randomGLM(x=X_train, y=y_train, xtest = X_test, nBags = bags)
-  model_rf_bigmtry = randomForest(x = X_train, y = as.factor(y_train),ntree = 500, mtry = ncol(X_train))
-  model_rf = randomForest(x = X_train, y = as.factor(y_train), ntree = 500)
-  model_xtree = extraTrees(x = X_train, y = as.factor(y_train), ntree = 500)
-  model_svm = svm(x = X_train, y = as.factor(y_train))
+  model_rf_bigmtry = randomForest(x = X_train, y = y_train, ntree = 500, mtry = ncol(X_train))
+  model_rf = randomForest(x = X_train, y = y_train, ntree = 500)
+  model_xtree = extraTrees(x = X_train, y = y_train, ntree = 500)
+  model_svm = svm(x = X_train, y = y_train)
   
-  accuracy_rglm[i] = 1 - sum(abs(model_rglm$predictedTest - y_test))/length(y_test)
-  accuracy_rf_bigmtry[i] = 1 - sum(abs(as.numeric(predict(model_rf_bigmtry, newdata=X_test)) - 1 - y_test))/length(y_test)
-  accuracy_rf[i] = 1 - sum(abs(as.numeric(predict(model_rf, newdata=X_test)) - 1 - y_test))/length(y_test)
-  accuracy_xtree[i] = 1 - sum(abs(as.numeric(predict(model_xtree, newdata=X_test)) - 1 - y_test))/length(y_test)
-  accuracy_svm[i] =  1 - sum(abs(as.numeric(predict(model_svm, newdata=X_test)) - 1 - y_test))/length(y_test)
+  accuracy_rglm[i] = sqrt(mean((log(model_rglm$predictedTest) - log(y_test))^2))
+  accuracy_rf_bigmtry[i] = sqrt(mean((log(predict(model_rf_bigmtry, newdata=X_test))- log(y_test))^2))
+  accuracy_rf[i] = sqrt(mean((log(predict(model_rf, newdata=X_test))- log(y_test))^2))
+  accuracy_xtree[i] = sqrt(mean((log(predict(model_xtree, newdata=X_test))- log(y_test))^2))
+  accuracy_svm[i] = sqrt(mean((log(predict(model_svm, newdata=X_test))- log(y_test))^2))
 }
+
+mean(accuracy_rglm)
+mean(accuracy_rf_bigmtry)
+mean(accuracy_rf)
+mean(accuracy_xtree)
+mean(accuracy_svm)
+
 
 
 
